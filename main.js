23c4c3ac348d8cbd1c60c697a426f110011feccb
Kaynec <68915270+Kaynec@users.noreply.github.com>
2021-01-22 17:48:49 +0330

    Add files via upload
    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// The html elements
+const container = document.querySelector("#container");
+const btn = document.querySelector('#btn')
+// initial grid 
+makeGrid(16);
+
+
+
+function makeGrid(value){
+    container.setAttribute('style',`grid-template:repeat(${value},1fr) / repeat(${value},1fr)`)
+    for (let i=0;i<Math.pow(value,2);i++){
+    const div =  document.createElement('div');
+    container.appendChild(div);
+    div.classList.add('div');
+   div.addEventListener('mouseover',(e)=>{
+   div.classList.add('black')
+})
+
+    
+}    
+}
+function removepixel(){
+    const newdiv = document.querySelectorAll('.div')
+    for (let i =0;i<newdiv.length;i++){
+        newdiv[i].remove()
+    }
+}
+
+
+const newdiv = document.querySelectorAll(".div");
+
+btn.addEventListener('click',(e)=>{
+    newdiv.forEach(div=>{
+    div.classList.remove('black')
+    })
+    removepixel()
+    let question = prompt('choose grid size from 1 to 100')
+    let answer=question.toLowerCase()
+    makeGrid(answer);
+})
+
+
+
+
